09a3452dfbe6c3d1408d92ddf2f3c7867ef1d8a5
pkasu06 <pkasu06@gmail.com>
2024-06-05 14:20:53 -0500

    added header, footer

---
+++ Portfolio-Project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="footer-container border-t-[1px]">
+            <p className="text-center">
+                Copyright ©2022 All rights reserved
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ Portfolio-Project/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import '../index.css'
+import { navItems } from '../constants'
+
+function Header() {
+
+    // const navigate = useNavigate();
+    //TODO: Add them changer
+
+    return (
+        <>
+            <header>
+                <nav className="header-container flex items-center justify-between flex-wrap p-6 border-b-[1px] border-white">
+                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                        {/* <Link to="/"> */}
+                            <h3 className="text-3xl font-extrabold text-primary">Portfolio</h3>
+                        {/* </Link> */}
+                    </div>
+                    <div className="block lg:hidden">
+                        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                        </button>
+                    </div>
+                    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                        <ul className="flex ml-auto">
+                            {navItems.map((item) => 
+                            navItems.length > 0 ? (
+                                <li key={item}>
+                                    <button className="inline-block px-6 py-2 duration-220 hover:bg-blue-100 rounded-full">{item}</button>
+                                </li>
+                            ) : null
+                            )}
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
