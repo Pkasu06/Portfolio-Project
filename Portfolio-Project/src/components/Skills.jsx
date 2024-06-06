bd2e5f75c5fe10632f857a7611c00d71d43bf242
pkasu06 <pkasu06@gmail.com>
2024-06-05 19:13:26 -0500

    changed classname, id for Skills component

--- Portfolio-Project/src/components/Skills.jsx
+++ Portfolio-Project/src/components/Skills.jsx
@@ -3,8 +3,8 @@ import { skillSet } from '../constants'
 
 function Skills() {
     return (
-        <div className="container text-center w-full p-4 my-20 flex flex-col items-center gap-y-4 md:max-w-7xl mx-auto md:flex-row md:items-center0 justify between" id="About">
-            <div className="about w-full p-2 rounded-lg border border-blue-800 bg-card text-card-foreground shadow-sm text-left">
+        <div className="container text-center w-full p-4 my-20 flex flex-col items-center gap-y-4 md:max-w-7xl mx-auto md:flex-row md:items-center0 justify between" id="Skills">
+            <div className="skills w-full p-2 rounded-lg border border-blue-800 bg-card text-card-foreground shadow-sm text-left">
                 <h3 className="text-2xl font-semibold leading-none tracking-tight border-b-[1px] border-blue-800 p-2 mb-2 text-blue-800 text-center">Skills</h3>
                 <div className="content p-4 mt-2">
                     <ul className="skills-list">
