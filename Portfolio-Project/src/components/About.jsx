4499a98658d1677e04f98d648fe371dad551094f
pkasu06 <pkasu06@gmail.com>
2024-06-05 19:02:55 -0500

    made ui changes to home, about sections

--- Portfolio-Project/src/components/About.jsx
+++ Portfolio-Project/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
     return (
-        <div className="container text-center w-full p-4 mt-10 flex flex-col items-center gap-y-4 md:max-w-7xl mx-auto md:flex-row md:items-center0 justify between" id="About">
+        <div className="container text-center w-full p-4 my-20 flex flex-col items-center gap-y-4 md:max-w-7xl mx-auto md:flex-row md:items-center0 justify between" id="About">
             <div className="about w-full p-2 rounded-lg border border-blue-800 bg-card text-card-foreground shadow-sm text-left">
                 <h3 className="text-2xl font-semibold leading-none tracking-tight border-b-[1px] border-blue-800 p-2 mb-2 text-blue-800 text-center">About me</h3>
                 <div className="content px-2 mt-2">
